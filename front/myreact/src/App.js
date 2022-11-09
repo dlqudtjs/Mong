646e8e80af7c12353aa72f1a61f44d81cbdd8ef4
dlqudtjs <dlqudtjs730@naver.com>
2022-11-09 22:26:09 +0900

    새로운 로그인, 회원가입 기능 구현

--- front/myreact/src/App.js
+++ front/myreact/src/App.js
@@ -4,11 +4,13 @@ import RegisterPage from "./components/RegisterPage";
 import MainPage from "./components/MainPage";
 import WritePage from "./components/WritePage";
 import ListViewPage from "./components/ListViewPage";
+import RandingPage from "./components/RandingPage";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Route exact path="/randing" component={RandingPage} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={RegisterPage} />
       <Route exact path="/main" component={MainPage} />
